88486e7013ab84a0236174d30fb8558536e5ad51
MrShawn <thunderboy288@gmail.com>
2020-05-27 23:23:31 -0400

    Added OfflinePlayer utils to PlayerUtils.java

--- src/main/java/dev/mrshawn/cronus/api/utils/PlayerUtils.java
+++ src/main/java/dev/mrshawn/cronus/api/utils/PlayerUtils.java
@@ -1,6 +1,7 @@
 package dev.mrshawn.cronus.api.utils;
 
 import dev.mrshawn.cronus.API;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 public class PlayerUtils {
@@ -11,6 +12,14 @@ public class PlayerUtils {
 
 	public static Player getOnlinePlayer(String playerName) {
 		return API.getPlugin().getServer().getPlayer(playerName);
+	}
+
+	public static boolean isOfflinePlayer(String playerName) {
+		return getOfflinePlayer(playerName).hasPlayedBefore();
+	}
+
+	public static OfflinePlayer getOfflinePlayer(String playerName) {
+		return API.getPlugin().getServer().getOfflinePlayer(playerName);
 	}
 
 }
