1fee34de72d0d3848fa37b7880e8aedd2e395f96
MrShawn <thunderboy288@gmail.com>
2020-05-27 23:06:59 -0400

    Improved YMLFile reload method

--- src/main/java/dev/mrshawn/cronus/api/files/YMLFile.java
+++ src/main/java/dev/mrshawn/cronus/api/files/YMLFile.java
@@ -1,19 +1,25 @@
 package dev.mrshawn.cronus.api.files;
 
+import com.google.common.base.Charsets;
 import dev.mrshawn.cronus.API;
 import lombok.Getter;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @Getter
 public class YMLFile {
 
+	private String name;
 	private File file;
 	private YamlConfiguration config;
 
 	public YMLFile(String name, boolean resource) {
+		this.name = name;
 		file = new File(API.getPlugin().getDataFolder() + File.separator + name + ".yml");
 		if (resource) {
 			API.getPlugin().saveResource(name + ".yml", false);
@@ -29,8 +35,12 @@ public class YMLFile {
 		config = YamlConfiguration.loadConfiguration(file);
 	}
 
-	public void reload() {
-		config = YamlConfiguration.loadConfiguration(file);
+	public void reload(Plugin plugin) {
+		YamlConfiguration newConfig = YamlConfiguration.loadConfiguration(file);
+		InputStream defConfigStream = plugin.getResource(name + ".yml");
+		if (defConfigStream != null) {
+			newConfig.setDefaults(YamlConfiguration.loadConfiguration(new InputStreamReader(defConfigStream, Charsets.UTF_8)));
+		}
 	}
 
 	public void save() {
