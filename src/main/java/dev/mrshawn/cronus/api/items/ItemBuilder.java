50ad8bf14c103d12f28c20c3db058992114dcfe9
Shawn11564 <thunderboy288@gmail.com>
2020-07-16 23:22:51 -0400

    Improved ItemBuilder and SkullBuilder

--- src/main/java/dev/mrshawn/cronus/api/items/ItemBuilder.java
+++ src/main/java/dev/mrshawn/cronus/api/items/ItemBuilder.java
@@ -1,5 +1,6 @@
 package dev.mrshawn.cronus.api.items;
 
+import de.tr7zw.changeme.nbtapi.NBTItem;
 import dev.mrshawn.cronus.api.utils.Chat;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,57 +10,79 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Getter
-@Setter
 public class ItemBuilder {
 
-	protected ItemStack item;
-	protected Material material;
-	protected ItemMeta meta;
-	protected int amount;
-	protected short damage;
-	protected List<String> lore;
-	protected String name;
-	protected Map<Enchantment, Integer> enchantments = new HashMap<>();
+	private ItemStack item;
+	private Material material;
+	private int amount;
+	private ItemMeta meta;
+	private List<String> lore = new ArrayList<>();
+	private Map<Enchantment, Integer> enchants = new HashMap<>();
+	private NBTItem nbtItem;
 
 	public ItemBuilder(Material material) {
 		this.material = material;
 		amount = 1;
-		damage = (short) 0;
-		meta = Bukkit.getItemFactory().getItemMeta(material);
+		item = new ItemStack(this.material, amount);
+		meta = item.getItemMeta();
+		nbtItem = new NBTItem(item);
 	}
 
 	public ItemBuilder(Material material, int amount) {
 		this.material = material;
 		this.amount = amount;
-		damage = (short) 0;
-		meta = Bukkit.getItemFactory().getItemMeta(material);
+		item = new ItemStack(this.material, amount);
+		meta = item.getItemMeta();
+		nbtItem = new NBTItem(item);
 	}
 
-	public ItemBuilder(Material material, int amount, short damage) {
-		this.material = material;
-		this.amount = amount;
-		this.damage = damage;
-		meta = Bukkit.getItemFactory().getItemMeta(material);
+	public ItemBuilder setName(String name) {
+		meta.setDisplayName(Chat.colorize(name));
+		return this;
 	}
 
-	public void addLoreLine(String line) {
+	public ItemBuilder addLoreLine(String line) {
 		lore.add(Chat.colorize(line));
+		return this;
 	}
+
+	public ItemBuilder addEnchant(Enchantment type, int level) {
+		enchants.put(type, level);
+		return this;
+	}
+
+	public ItemBuilder setAmount(int amount) {
+		this.amount = amount;
+		item.setAmount(this.amount);
+		return this;
+	}
+
+	public ItemBuilder addNBT(String key, String value) {
+		nbtItem.setString(key, value);
+		return this;
+	}
+
+	public ItemBuilder addNBT(String key, int value) {
+		nbtItem.setInteger(key, value);
+		return this;
+	}
+
+	public ItemBuilder addNBT(String key, boolean value) {
+		nbtItem.setBoolean(key, value);
+		return this;
+	}
+
 
 	public ItemStack build() {
-		item = new ItemStack(material, amount, damage);
-		meta.setDisplayName(Chat.colorize(name));
-		meta.setLore(Chat.colorizeList(lore));
+		meta.setLore(lore);
+		enchants.forEach((key, value) -> item.addUnsafeEnchantment(key, value));
 		item.setItemMeta(meta);
-		for (Map.Entry<Enchantment, Integer> entry : enchantments.entrySet()) {
-			item.addUnsafeEnchantment(entry.getKey(), entry.getValue());
-		}
 		return item;
 	}
-
 }
--- src/main/java/dev/mrshawn/cronus/api/items/SkullBuilder.java
+++ src/main/java/dev/mrshawn/cronus/api/items/SkullBuilder.java
@@ -16,31 +16,24 @@ public class SkullBuilder extends ItemBuilder {
 
 	public SkullBuilder(Material material) {
 		super(material);
-		skullMeta = (SkullMeta) Bukkit.getItemFactory().getItemMeta(material);
+		skullMeta = (SkullMeta) getMeta();
 	}
 
 	public SkullBuilder(Material material, int amount) {
 		super(material, amount);
-		skullMeta = (SkullMeta) Bukkit.getItemFactory().getItemMeta(material);
+		skullMeta = (SkullMeta) getMeta();
 	}
 
-	public SkullBuilder(Material material, int amount, short damage) {
-		super(material, amount, damage);
-		skullMeta = (SkullMeta) Bukkit.getItemFactory().getItemMeta(material);
+	public SkullBuilder setOwner(UUID owner) {
+		skullMeta.setOwningPlayer(Bukkit.getOfflinePlayer(owner));
+		return this;
 	}
 
-	public void setOwner(UUID uuid) {
-		skullMeta.setOwningPlayer(Bukkit.getOfflinePlayer(uuid));
-	}
-
+	@Override
 	public ItemStack build() {
-		item = new ItemStack(material, amount, damage);
-		skullMeta.setDisplayName(Chat.colorize(name));
-		skullMeta.setLore(Chat.colorizeList(lore));
-		item.setItemMeta(skullMeta);
-		for (Map.Entry<Enchantment, Integer> entry : enchantments.entrySet()) {
-			item.addUnsafeEnchantment(entry.getKey(), entry.getValue());
-		}
-		return item;
+		skullMeta.setLore(getLore());
+		getEnchants().forEach((key, value) -> getItem().addUnsafeEnchantment(key, value));
+		getItem().setItemMeta(skullMeta);
+		return getItem();
 	}
 }
